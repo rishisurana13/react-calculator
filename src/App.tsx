04d5aa3286b563061ed74dd5a57023ae0464dd0b
rishisurana13 <rishisurana@hotmail.com>
2023-01-09 18:57:00 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Grid, Typography } from "@mui/material";
+import {evaluate } from "mathjs";
+import { useState } from "react";
 
 function App() {
+  const [displayValue, setDisplayValue] = useState("0");
+  const [evaluationString, setEvaluationString] = useState("");
+
+  const buttons = [
+    "7",
+    "8",
+    "9",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "*",
+    "0",
+    ".",
+    "=",
+    "/",
+    "CLEAR",
+  ];
+  const operands = ["+", "-", "/", "*", "CLEAR", "="];
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const buttonValue = event.currentTarget.innerText;
+    
+    if (!operands.includes(buttonValue)) {
+      // support multi-digit numbers
+      if (displayValue !== "0") {
+        setDisplayValue(displayValue + buttonValue);
+      } else {
+        setDisplayValue(buttonValue);
+      }
+    }
+
+    if (buttonValue === "=") {
+      const v = evaluate(evaluationString);
+      console.log("result:" + v);
+      setDisplayValue(v);
+      return;
+    }
+
+    if (buttonValue === "CLEAR") {
+      setDisplayValue("0");
+      setEvaluationString("");
+      return;
+    }
+
+    if (buttonValue !== "CLEAR") {
+      setEvaluationString(evaluationString + buttonValue);
+
+    }
+    console.log(evaluationString);
+    const lastButtonClicked = evaluationString.charAt(evaluationString.length-1)
+    
+    if (operands.includes(lastButtonClicked)) {
+      setDisplayValue(buttonValue)
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box
+        sx={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Grid width={"20%"} minWidth={'300px'}>
+        <Grid item xs={12}>
+          <Typography variant="h4" align="right">
+            {displayValue}
+          </Typography>
+        </Grid>
+          <Grid container spacing={1}>
+            {buttons.map((button) => (
+              <Grid item xs={3} key={button}>
+                <Button
+                  variant="outlined"
+                  fullWidth
+                  value={button}
+                  onClick={handleClick}
+                >
+                  {button}
+                </Button>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
